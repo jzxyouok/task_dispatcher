e034b0cc744896ed9f98ad724ba376fcae796247
jlh321 <630987606@qq.com>
2019-05-06 21:12:31 +0800

    后台管理框架

---
+++ taskdispatchervue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="100">
+    <h1>任务发布后台系统</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TimePicker/>
+    <Table/>
+  </div>
+</template>
+
+<script>
+
+import TimePicker from './components/TimePicker.vue'
+import Table from './components/Table.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TimePicker,
+    Table
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+
+</style>
---
+++ taskdispatchervue/src/components/Table.vue
@@ -0,0 +1,70 @@
+<template>
+  <el-table
+    :data="tableData"
+    style="width: 100%">
+
+    <el-table-column
+      prop="index"
+      label="序号"
+      width="50">
+    </el-table-column>
+    <el-table-column
+      prop="project"
+      label="项目名称"
+      width="150">
+    </el-table-column>
+    <el-table-column
+      prop="manager"
+      label="项目负责人"
+      width="150">
+    </el-table-column>
+   <el-table-column
+      prop="day"
+      label="有效人天数"
+      width="150">
+    </el-table-column>
+     <el-table-column
+      prop="proportion"
+      label="项目权重"
+      width="150">
+    </el-table-column>
+     <el-table-column label="参与者权重">
+        <el-table-column
+        prop="name1"
+        label="李想"
+        width="120">
+      </el-table-column>
+
+     </el-table-column>
+  </el-table>
+</template>
+
+<script>
+  export default {
+    name: 'Table',
+    data() {
+      return {
+        tableData: [{
+          index:1,
+          project:'高速公路智能环保监控系统开发',
+          manager:'柏玉锋',
+          day:'12',
+          proportion:'0.12',
+          name1:'12'
+        },
+        {
+          index:2,
+          project:'高速公路智能环保监控系统开发',
+          manager:'柏玉锋',
+          day:'12',
+          proportion:'0.12',
+          name1:'12'
+        }
+        
+        
+        
+        ]
+      }
+    }
+  }
+</script>
