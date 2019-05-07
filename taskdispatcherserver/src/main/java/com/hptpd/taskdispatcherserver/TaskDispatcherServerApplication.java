2395ed0aaa5a38be90092f99cc76db85a8858d16
liucheng1025 <854238031@qq.com>
2019-05-07 17:28:43 +0800

    :sparkles: 跨域问题

--- taskdispatcherserver/src/main/java/com/hptpd/taskdispatcherserver/TaskDispatcherServerApplication.java
+++ taskdispatcherserver/src/main/java/com/hptpd/taskdispatcherserver/TaskDispatcherServerApplication.java
@@ -8,15 +8,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 /**
  * @author ymhui
  */
 @SpringBootApplication
-public class TaskDispatcherServerApplication {
+public class TaskDispatcherServerApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskDispatcherServerApplication.class, args);
+    }
+
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALL)
+                .allowedMethods(ALL)
+                .allowedHeaders(ALL)
+                .allowCredentials(true);
     }
 
     /**
